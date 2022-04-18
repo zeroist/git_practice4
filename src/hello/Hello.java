5641c118f6b27b91912e047b2344ff2c016aeeff
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-04-18 16:06:05 +0300

    doing final work hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,6 @@ package hello;
 public class Hello  {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("b25 learning git and githupb");
     }
 }
